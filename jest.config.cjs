7096e85f65a4dcaa921fd0646adfc71205b81382
vinodabhik-proj <vinodabhik@gmail.com>
2025-11-26 23:02:55 +0000

    Fixing naming discrepency in setup files and config and removing prior changes

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  rootDir: "../rovers-frontend",
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/tests/fileMock.js"
