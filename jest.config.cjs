8f042206bbc656848253603d5cf39ab90a9e5771
vinodabhik-proj <vinodabhik@gmail.com>
2025-11-26 22:59:44 +0000

    Adding root directory fix in jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  rootDir: "rovers-frontend",
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/tests/fileMock.js"
