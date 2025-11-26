485d8445a7c088b9aecbb85d8c071c7a27826229
vinodabhik-proj <vinodabhik@gmail.com>
2025-11-26 23:01:00 +0000

    Adding repository fix for jest config by moving into parent

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  rootDir: "rovers-frontend",
+  rootDir: "../rovers-frontend",
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/tests/fileMock.js"
