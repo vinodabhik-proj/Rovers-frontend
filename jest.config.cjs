47cf538fbb77961d775975f23e9622664b7136ef
vinodabhik-proj <vinodabhik@gmail.com>
2025-11-30 16:57:13 +0000

    Adding test ts config file and modifying jest config to point to it

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,11 @@ module.exports = {
   preset: "ts-jest",
   rootDir: ".",
   testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json"
+    }
+  },
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/tests/fileMock.js"
