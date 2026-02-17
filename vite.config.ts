7b7029076c0cd4c18b944dc91395223764585fcb
vinodabhik-proj <vinodabhik@gmail.com>
2026-02-17 20:50:06 +0000

    Adding environment flag on vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,25 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../.cert/localhost.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../.cert/localhost.pem')),
-    },
-    port: 5173
+export default defineConfig(({ mode }) => {
+  const isLocalDev = mode === 'development'
+
+  return {
+    plugins: [react()],
+
+    server: isLocalDev
+      ? {
+          https: {
+            key: fs.readFileSync(
+              path.resolve(__dirname, '../.cert/localhost.key')
+            ),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, '../.cert/localhost.pem')
+            ),
+          },
+          port: 5173,
+        }
+      : undefined,
   }
 })
+
